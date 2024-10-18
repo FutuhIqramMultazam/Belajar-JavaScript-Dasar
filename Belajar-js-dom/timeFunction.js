a49214ffc13294bf841c550834f61469c2eaaca0
FutuhIqramMultazam <futuhiqramm@gmail.com>
2024-10-18 23:36:40 +0700

    telah menyelesaikan materi traversal, preventDefault dan Bubbling

---
+++ Belajar-js-dom/timeFunction.js
@@ -0,0 +1,23 @@
+// setTimeout ########
+
+// const set = setTimeout(function () {
+//   console.log("hello icam");
+// }, 5000 /*param kedua untuk set detik, ini saya menuliskan 3 detik*/);
+
+// document.getElementById("tombol").addEventListener("click", function () {
+//   clearTimeout(set);
+//   console.log("setTime Out di berhentikan");
+// });
+
+// setInterval #######
+
+/* const set = setInterval(function () {
+  console.log("Hello coy");
+}, 2000);
+
+document.getElementById("tombol").addEventListener("click", function () {
+  clearInterval(set);
+  console.log("setInterval di berhentikan");
+}); */
+
+// Program hitung mundur
